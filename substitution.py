abcd1617ecd555b1b16714db65c296a2576438ed
katkadeshubham58 <katkadeshubham58@gmail.com>
2026-05-08 09:50:32 +0530

    Update substitution.py

--- substitution.py
+++ substitution.py
@@ -1,50 +1,34 @@
-def get_order(key):
-    return sorted(range(len(key)),key=lambda k:key[k])
-    
 def encrypt(text,key):
-    col=len(key)
-    row=(len(text)+col-1)//col
-    
-    text+='x'*(row*col-len(text))
-    
-    mat=[]
-    for i in range(0,len(text),col):
-        mat.append(text[i:i+col])
-    
-    cipher=""
-    for c in get_order(key):
-        for r in range(row):
-            cipher+=mat[r][c]
-            
-    return cipher
-    
-def decrypt(text,key):
-    col=len(key)
-    row=len(text)//col
-    
-    mat=[[""for _ in range(col)]for _ in range(row)]
-    
-    k=0
-    for c in get_order(key):
-        for r in range(row):
-            mat[r][c]=text[k]
-            k+=1
-            
-    
     result=""
-    for r in mat:
-        result+=''.join(r)
-    return result.rstrip('x') 
+    for ch in text:
+        if ch.isalpha():
+            if ch.isupper():
+                result+=chr((ord(ch)-ord('A')+key)%26+ord('A'))
+            else:
+                result+=chr((ord(ch)-ord('a')+key)%26+ord('a'))
+        else:
+            result+=ch
+    return result        
+                
+def decrypt(text,key):
+    result=""
+    for ch in text:
+        if ch.isalpha():
+            if ch.isupper():
+                result+=chr((ord(ch)-ord('A')-key)%26+ord('A'))
+            else:
+                result+=chr((ord(ch)-ord('a')-key)%26+ord('a'))
+        else:
+            result+=ch
+    return result      
     
-text=input("Enter message:")
-key=input("Enter key:")
+text=input("Enter data:")
+key=int(input("Enter key:"))
 
-clean=text.replace(" ","").lower()
-
-encrypted=encrypt(clean,key)
+encrypted=encrypt(text,key)
 print("Encrypted:",encrypted)
 
 decrypted=decrypt(encrypted,key)
 print("Decrypted:",decrypted)
-        
+    
     
